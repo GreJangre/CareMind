72c0a53d91d237613c73433eedd151891f143eb1
GreJangre <dlatjdwn8824@naver.com>
2019-05-26 23:21:38 +0900

    SpinWheelController 구현

--- CareMind/AppDelegate.swift
+++ CareMind/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import SpinWheelControl
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- CareMind/ViewController.swift
+++ CareMind/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import SpinWheelControl
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SpinWheelControlDataSource, SpinWheelControlDelegate {
+    let colorPalette: [UIColor] = [UIColor.brown, UIColor.darkGray, UIColor.red, UIColor.orange, UIColor.black, UIColor.gray, UIColor.lightGray,]
+    
+    func wedgeForSliceAtIndex(index: UInt) -> SpinWheelWedge {
+        let wedge = SpinWheelWedge()
+        
+        wedge.shape.fillColor = colorPalette[Int(index)].cgColor
+        wedge.label.text = "Label #" + String(index)
+        
+        return wedge
+    }
+    
+    var spinWheelControl: SpinWheelControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        
+        spinWheelControl = SpinWheelControl(frame: frame, snapOrientation: .up, wedgeLabelOrientation: WedgeLabelOrientation.around)
+        spinWheelControl.addTarget(self, action: #selector(spinWheelDidChangeValue), for: UIControl.Event.valueChanged)
+        
+        spinWheelControl.dataSource = self
+        spinWheelControl.reloadData()
+        
+        spinWheelControl.delegate = self
+        
+        self.view.addSubview(spinWheelControl)
     }
-
-
+    
+    func numberOfWedgesInSpinWheel(spinWheel: SpinWheelControl) -> UInt {
+        return 7
+    }
+    
+    func spinWheelDidEndDecelerating(spinWheel: SpinWheelControl) {
+        print("The spin wheel did end decelerating")
+    }
+    
+    func spinWheelDidRotateByRadians(radians: Radians) {
+        print("The wheel did rotate this many radians - " + String(describing: radians))
+    }
+    
+    @objc func spinWheelDidChangeValue(sender: AnyObject) {
+        print("Value changed to " + String(self.spinWheelControl.selectedIndex))
+//        let uv = self.storyboard?.instantiateViewController(withIdentifier: "_Profile")
+//        self.present(uv!, animated: true)
+    }
 }
 
