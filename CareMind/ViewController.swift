9d5d9ce65a645a6a6dd8d00bcad8400a1e7ff29c
GreJangre <dlatjdwn8824@naver.com>
2019-05-26 19:38:12 +0900

    Initial Commit

---
+++ CareMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CareMind
+//
+//  Created by 임성주 on 26/05/2019.
+//  Copyright © 2019 jangre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
